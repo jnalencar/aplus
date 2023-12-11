a5534f505e94b59288bb1769c5c228c080ca76fa
Jonathan Neves <jonathannalencar@gmail.com>
2023-12-11 11:18:47 -0300

    cors now working

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,18 @@ const path = require('path');
 const port = 3000;
 const mongoose = require('mongoose');
 const routes = require('./routes'); // Importa as rotas
+const cors = require('cors');
 
+app.use(cors());
+
+//rotas anteriores
 app.use('/database/movies', express.static(path.join(__dirname, 'database/movies')));
-
 app.use('/database/series/t1', express.static(path.join(__dirname, 'database/series/t1')));
 app.use('/database/series/t2', express.static(path.join(__dirname, 'database/series/t2')));
 app.use('/database/series/t3', express.static(path.join(__dirname, 'database/series/t3')));
 app.use('/database/series/t4', express.static(path.join(__dirname, 'database/series/t4')));
 app.use('/database/series/t5', express.static(path.join(__dirname, 'database/series/t5')));
+//fim rotas anteriores
 
 app.use('/', routes); // Usa as rotas importadas
 
