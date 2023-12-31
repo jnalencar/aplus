5025a9fc3aa717bdd1d49bf47f07d5b56a9fb40c
Jonathan Alencar <jonathannalencar@gmail.com>
2023-12-31 19:11:21 -0300

    Add carousel and scroll functionality to movie and series galleries

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -26,3 +26,62 @@ fetch('http://localhost:3000/movies')
         });
     })
     .catch(error => console.error('Error:', error));
+
+const sliderM = document.querySelector('.movie-gallery');
+let isDownM = false;
+let startXM;
+let scrollLeftM;
+
+sliderM.addEventListener('mousedown', eM => {
+  isDownM = true;
+  sliderM.classList.add('active');
+  startXM = eM.pageX - sliderM.offsetLeft;
+  scrollLeftM = sliderM.scrollLeft;
+});
+sliderM.addEventListener('mouseleave', _ => {
+  isDownM = false;
+  sliderM.classList.remove('active');
+});
+sliderM.addEventListener('mouseup', _ => {
+  isDownM = false;
+  sliderM.classList.remove('active');
+});
+sliderM.addEventListener('mousemove', eM => {
+  if (!isDownM) return;
+  eM.preventDefault();
+  const x = eM.pageX - sliderM.offsetLeft;
+  const SCROLL_SPEED = 1;
+  const walk = (x - startXM) * SCROLL_SPEED;
+  sliderM.scrollLeft = scrollLeftM - walk;
+});
+
+const sliderS = document.querySelector('.series-gallery');
+let isDownS = false;
+let startXS;
+let scrollLeftS;
+
+sliderS.addEventListener('mousedown', eS => {
+  isDownS = true;
+  sliderS.classList.add('active');
+  startXS = eS.pageX - sliderS.offsetLeft;
+  scrollLeftS = sliderS.scrollLeft;
+});
+
+sliderS.addEventListener('mouseleave', _ => {
+  isDownS = false;
+  sliderS.classList.remove('active');
+});
+
+sliderS.addEventListener('mouseup', _ => {
+    isDownS = false;
+    sliderS.classList.remove('active');
+});
+
+sliderS.addEventListener('mousemove', eS => {
+    if (!isDownS) return;
+    eS.preventDefault();
+    const x = eS.pageX - sliderS.offsetLeft;
+    const SCROLL_SPEED = 1;
+    const walk = (x - startXS) * SCROLL_SPEED;
+    sliderS.scrollLeft = scrollLeftS - walk;
+});
